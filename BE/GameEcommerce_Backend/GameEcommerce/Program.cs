4f508b3561e25307a60277f5bcd1b14416336b55
nguyenphuongkiet <KietNPSE172931@fpt.edu.vn>
2024-04-18 03:22:28 +0700

    Add super admin, modify authentication

--- BE/GameEcommerce_Backend/GameEcommerce/Program.cs
+++ BE/GameEcommerce_Backend/GameEcommerce/Program.cs
@@ -1,4 +1,7 @@
+using BusinessObject;
 using DataAccessObject;
+using DataAccessObject.Seed;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -13,6 +17,18 @@ builder.Services.AddDbContext<GameEcommerceContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddIdentityCore<User>(opt =>
+{
+    opt.Password.RequiredLength = 5;
+
+    opt.User.RequireUniqueEmail = true;
+})
+    .AddRoles<Role>()
+    .AddRoleManager<RoleManager<Role>>()
+    .AddEntityFrameworkStores<GameEcommerceContext>()
+    .AddSignInManager<SignInManager<User>>()
+    .AddDefaultTokenProviders();
+builder.Services.AddAuthentication();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,7 +41,23 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.UseAuthentication();
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<GameEcommerceContext>();
+    var userManager = services.GetRequiredService<UserManager<User>>();
+    var roleManager = services.GetRequiredService<RoleManager<Role>>();
+    await context.Database.MigrateAsync();
+/*    await context.Database.ExecuteSqlRawAsync("DELETE FROM Connections");
+*/    await Seed.SeedUser(userManager, roleManager);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error while seeding data");
+}
 app.Run();
